b209df95b425f2f899ad292f7fcc27d4dbeddc63
JustSiHeshteger <siheshteger@gmail.com>
2023-02-01 20:41:30 +0300

    equals/hashcode

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -18,38 +18,38 @@ public class UserController {
     }
 
     @GetMapping("/")
-    public String getAllUsers(Model model) {
+    public String openPageWithAllUsers(Model model) {
         model.addAttribute("allUsers", userService.getAllUsers());
         return "users";
     }
 
     @GetMapping("/new")
-    public String addingNewUser(Model model) {
+    public String openPageForAddNewUser(Model model) {
         model.addAttribute("user", new User());
         return "add_user";
     }
 
     @PostMapping("/addUser")
-    public String savingUser(@ModelAttribute("user") User user) {
+    public String saveNewUserToDb(@ModelAttribute("user") User user) {
         userService.addUser(user);
         return "redirect:/";
     }
 
     @GetMapping("/editUser/{userId}")
-    public String editingUser(@PathVariable("userId") int id, Model model){
+    public String openPageForEditUser(@PathVariable("userId") int id, Model model){
         model.addAttribute("user", userService.getUserById(id));
         return "edit_user";
     }
 
     @PatchMapping(value="/editUser/{getId}")
-    public String saveEditedUser(@PathVariable int getId, @ModelAttribute("user") User user){
+    public String saveEditedUserToDb(@PathVariable int getId, @ModelAttribute("user") User user){
         user.setId(getId);
         userService.updateUser(user);
         return "redirect:/";
     }
 
     @DeleteMapping("deleteUser/{getId}")
-    public String deleteUser(@PathVariable int getId) {
+    public String deleteUserFromDb(@PathVariable int getId) {
         userService.deleteUser(getId);
         return "redirect:/";
     }
--- src/main/java/com/example/springboot/entity/User.java
+++ src/main/java/com/example/springboot/entity/User.java
@@ -77,22 +77,11 @@ public class User {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof User user)) return false;
-
-        if (id != user.id) return false;
-        if (!Objects.equals(name, user.name)) return false;
-        if (!Objects.equals(lastName, user.lastName)) return false;
-        if (!Objects.equals(email, user.email)) return false;
-
-        return Objects.equals(country, user.country);
+        return id == user.id;
     }
 
     @Override
     public int hashCode() {
-        int result = id;
-        result += 31 * result + (name != null ? name.hashCode() : 0);
-        result += 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result += 31 * result + (email != null ? email.hashCode() : 0);
-        result += 31 * result + (country != null ? country.hashCode() : 0);
-        return result;
+        return Objects.hash(id);
     }
 }
